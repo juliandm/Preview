11a1ada56bc2b86dfde044c100eb754953b2d4b0
Julian <juliandemourgues@gmail.com>
2018-06-18 18:49:06 +0200

    final integration nodemon

--- server/index.js
+++ server/index.js
@@ -15,7 +15,6 @@ var bodyParser = require('body-parser')
 const bcrypt = require('bcrypt');
 // If you need a backend, e.g. an API, add your custom backend-specific middleware here
 // app.use('/api', myApi);
-
 // In production we need to pass these values in instead of relying on webpack
 setup(app, {
   outputPath: resolve(process.cwd(), 'build'),
